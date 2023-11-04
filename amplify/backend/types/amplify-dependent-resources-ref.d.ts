1d38070a2222c5004ecbc415c4ab8c7b00902c2e
池田慎太郎 <ikeshi0015@icloud.com>
2023-11-04 13:55:31 +0900

    実装完了

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "awstutorialwithreact1e6829c4": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
